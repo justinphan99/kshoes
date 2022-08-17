565eb9f6efbaf0dfea297a028c082d2e3c6f5b24
pqkhoa <phanquockhoa99@gmail.com>
2022-08-17 15:12:58 +0700

    first commit

---
+++ products-service/main/config.py
@@ -0,0 +1,27 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'khoa1009')
+    DEBUG = False
+    RESTX_MASK_SWAGGER = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "postgresql://admin:admin@localhost:5432/products"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    prod=ProductionConfig
+)
+
+key = Config.SECRET_KEY
